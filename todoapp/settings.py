441c782ab4882a5ab7b0ced1bc490708538b5f8c
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-04 00:06:47 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = "/var/www/sf-pyw-django/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
